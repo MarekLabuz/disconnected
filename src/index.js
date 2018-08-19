9ca71db0180d4779e634e73840598231f1b68ce0
Marek Łabuz <mrk.labuz@gmail.com>
2018-08-19 17:29:02 +0200

    simple shapes

--- src/index.js
+++ src/index.js
@@ -1,39 +1,71 @@
 import Gruu from 'gruujs'
 import cx from 'classnames'
 
-import footSVG from './svg/foot.svg'
-import calfSVG from './svg/calf2.svg'
-import thighSVG from './svg/thigh.svg'
+// import footSVG from './svg/foot.svg'
+// import calfSVG from './svg/calf2.svg'
+// import thighSVG from './svg/thigh.svg'
+// import torsoSVG from './svg/torso.svg'
 import style from './index.scss'
 
-const Leg = (customStyle = {}) => (
-  <div className={style.leg}>
-    <div className={cx(style.legPart, style.thigh)} style={customStyle}>
-      <div innerHTML={thighSVG} />
-      <div className={cx(style.legPart, style.calf)} style={customStyle}>
-        <div innerHTML={calfSVG} />
-        <div className={cx(style.legPart, style.foot)} style={customStyle}>
-          <div innerHTML={footSVG} />
+const Leg = (customStyle = {}, customLegPartStyle = {}) => (
+  <div className={style.leg} style={customStyle}>
+    <div className={cx(style.legPart, style.thigh)} style={customLegPartStyle}>
+      {/* <div innerHTML={thighSVG} /> */}
+      <div className={cx(style.legPart, style.calf)} style={customLegPartStyle}>
+        {/* <div innerHTML={calfSVG} /> */}
+        <div className={cx(style.legPart, style.foot)} style={customLegPartStyle}>
+          {/* <div innerHTML={footSVG} /> */}
         </div>
       </div>
     </div>
   </div>
 )
 
+const Arm = (customStyle = {}, customArmPartStyle = {}) => (
+  <div className={style.arm} style={customStyle}>
+    <div className={cx(style.armPart, style.shoulder)}  style={customArmPartStyle}>
+      <div className={cx(style.armPart, style.forearm)}  style={customArmPartStyle} />
+    </div>
+  </div>
+)
+
+const Head = (
+  <div className={style.head} />
+)
+
+const Chest = (
+  <div className={style.chest}>
+    {Head}
+    {/* <div innerHTML={torsoSVG} /> */}
+    {Arm({ zIndex: 10 }, { animationDelay: 0 })}
+    {Arm({ zIndex: 30 }, { animationDelay: '-1s' })}
+  </div>
+)
+
 const Hero = (
   <div className={style.hero}>
-    <div className={style.torso}>
+    {Chest}
+    {Leg({ zIndex: 10 }, { animationDelay: 0 })}
+    {Leg({ zIndex: 30 }, { animationDelay: '-1s' })}
+  </div>
+)
+
+const Light = (
+  <div className={style.light}>
+    <div />
+    <div>
+      <div />
+      <div />
+      <div />
     </div>
-    <div className={style.legs}>
-      {Leg({ animationDelay: 0 })}
-      {Leg({ animationDelay: '-1s' })}
-    </div>
+    <div />
   </div>
 )
 
 const App = (
   <div>
     {Hero}
+    {Light}
   </div>
 )
 
