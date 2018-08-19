1f5c2960d9b729b01d081d09e47ff2d84089e551
Marek Łabuz <mrk.labuz@gmail.com>
2018-08-19 23:46:16 +0200

    house

--- src/index.js
+++ src/index.js
@@ -5,7 +5,27 @@ import cx from 'classnames'
 // import calfSVG from './svg/calf2.svg'
 // import thighSVG from './svg/thigh.svg'
 // import torsoSVG from './svg/torso.svg'
+import houseSVG from './svg/house4.svg'
+
 import style from './index.scss'
+import styleHouse from './house.scss'
+
+const PlayerStore = (
+  <$
+    state={{
+      direction: 'NONE'
+    }}
+  />
+)
+
+const HouseStore = (
+  <$
+    state={{
+      left: -1960,
+      top: -681
+    }}
+  />
+)
 
 const Leg = (customStyle = {}, customLegPartStyle = {}) => (
   <div className={style.leg} style={customStyle}>
@@ -43,7 +63,11 @@ const Chest = (
 )
 
 const Hero = (
-  <div className={style.hero}>
+  <div className={style.hero} $style={() => ({
+    transform: PlayerStore.state.direction === 'RIGHT'
+      ? 'scaleX(-1) translate(50%, -50%)'
+      : 'scaleX(1) translate(-50%, -50%)'
+  })}>
     {Chest}
     {Leg({ zIndex: 10 }, { animationDelay: 0 })}
     {Leg({ zIndex: 30 }, { animationDelay: '-1s' })}
@@ -51,7 +75,11 @@ const Hero = (
 )
 
 const Light = (
-  <div className={style.light}>
+  <div className={style.light} $style={() => ({
+    transform: PlayerStore.state.direction === 'RIGHT'
+      ? 'scaleX(-1)'
+      : 'scaleX(1)'
+  })}>
     <div />
     <div>
       <div />
@@ -62,11 +90,56 @@ const Light = (
   </div>
 )
 
+const HouseBackground = (
+  <div className={styleHouse.house} $style={() => ({
+    left: `${HouseStore.state.left}px`,
+    top: `${HouseStore.state.top}px`,
+  })}>
+    {/* <div className={styleHouse.computer}/> */}
+    <div innerHTML={houseSVG} />
+  </div>
+)
+
 const App = (
   <div>
+    {HouseBackground}
     {Hero}
     {Light}
   </div>
 )
 
 Gruu.renderApp(document.querySelector('#root'), App)
+
+const gameLoop = () => {
+  switch (PlayerStore.state.direction) {
+    case 'LEFT':
+      HouseStore.state.left += 5
+      break
+    case 'RIGHT':
+      HouseStore.state.left -= 5
+      break
+    default:
+      break
+  }
+  setTimeout(gameLoop, 30)
+}
+
+document.addEventListener('keydown', (e) => {
+  console.log(e.keyCode)
+  switch (e.keyCode) {
+    case 39:
+      PlayerStore.state.direction = 'RIGHT'
+      break
+    case 37:
+      PlayerStore.state.direction = 'LEFT'
+      break
+    default:
+      break
+  }
+})
+
+document.addEventListener('keyup', (e) => {
+  PlayerStore.state.direction = 'NONE'
+})
+
+gameLoop()
