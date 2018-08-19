7a793cd9649fad9d0737771154225b68d01c8c9c
Marek Łabuz <mrk.labuz@gmail.com>
2018-08-19 15:19:34 +0200

    legs

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import Gruu from 'gruujs'
 import cx from 'classnames'
 
 import footSVG from './svg/foot.svg'
-import calfSVG from './svg/calf.svg'
+import calfSVG from './svg/calf2.svg'
+import thighSVG from './svg/thigh.svg'
 import style from './index.scss'
 
 const Leg = (customStyle = {}) => (
   <div className={style.leg}>
     <div className={cx(style.legPart, style.thigh)} style={customStyle}>
+      <div innerHTML={thighSVG} />
       <div className={cx(style.legPart, style.calf)} style={customStyle}>
         <div innerHTML={calfSVG} />
         <div className={cx(style.legPart, style.foot)} style={customStyle}>
@@ -20,8 +22,12 @@ const Leg = (customStyle = {}) => (
 
 const Hero = (
   <div className={style.hero}>
-    {Leg({ animationDelay: 0 })}
-    {Leg({ animationDelay: '-1s' })}
+    <div className={style.torso}>
+    </div>
+    <div className={style.legs}>
+      {Leg({ animationDelay: 0 })}
+      {Leg({ animationDelay: '-1s' })}
+    </div>
   </div>
 )
 
